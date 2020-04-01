51d6e51d7e73b8fde9a77dc3b482dcb7d26a3b04
Ally Brannon <allison.brannon@gmail.com>
2020-04-01 11:53:52 -0400

    intial css updated

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const UserCard = ({ user: { login, avatar_url, html_url } }) => {
+const UserCard = ({ user: { login, avatar_url, html_url, repos_url } }) => {
   return (
     <div>
       <img src={avatar_url} alt="userpic" width="100px" />
       <a href={html_url}>
         <p>{login}</p>
+        <p>{repos_url}</p>
       </a>
     </div>
   );
